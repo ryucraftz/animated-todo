be7a190c2d2f1db49701834cc72dc94987936469
Kirito <vaibhavsathe0204@gmail.com>
2023-04-23 22:38:47 +0530

    intial Commit

---
+++ src/screens/about-screen.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import {
+  ScrollView,
+  Box,
+  Text,
+  VStack,
+  Icon,
+  Image,
+  useColorModeValue
+} from 'native-base'
+import { Feather } from '@expo/vector-icons'
+import AnimatedColorBox from '../components/animated-color-box'
+import Navbar from '../components/navbar'
+import Masthead from '../components/masthead'
+import LinkButton from '../components/link-button'
+
+const AboutScreen = () => {
+  return (
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue('warmGray.50', 'warmGray.900')}
+      w="full"
+    >
+      <Masthead
+        title="About this app"
+        image={require('../assets/about-masthead.png')}
+      >
+        <Navbar />
+      </Masthead>
+      <ScrollView
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        mt="-20px"
+        pt="30px"
+        p={4}
+      >
+        <VStack flex={1} space={4}>
+          <Box alignItems="center">
+            <Image
+              source={require('../assets/vaibhav.jpg')}
+              borderRadius="full"
+              resizeMode="cover"
+              w={120}
+              h={120}
+              alt="author"
+            />
+          </Box>
+          <Text fontSize="md" w="full">
+            This is a React Native tutorial built in the YouTube channel called
+            Idle_DEV.
+          </Text>
+          <LinkButton
+            colorScheme="red"
+            size="lg"
+            borderRadius="full"
+            href="https://www.youtube.com/channel/UCDTL3RRDT2F48dqW1KUfLlg"
+            leftIcon={
+              <Icon as={Feather} name="youtube" size="sm" opacity={0.5} />
+            }
+          >
+            Go to YouTube channel
+          </LinkButton>
+          <LinkButton
+            colorScheme={useColorModeValue('blue', 'darkBlue')}
+            size="lg"
+            borderRadius="full"
+            href="https://twitter.com/ryucraftz?s=20"
+            leftIcon={
+              <Icon as={Feather} name="twitter" size="sm" opacity={0.5} />
+            }
+          >
+            @ryucraftz
+          </LinkButton>
+          <Text fontSize="md" w="full">
+            Official Website
+          </Text>
+          <LinkButton
+            colorScheme="purple"
+            size="lg"
+            borderRadius="full"
+            href="https://twitter.com/ryucraftz?s=20"
+            leftIcon={
+              <Icon as={Feather} name="external-link" size="sm" opacity={0.5} />
+            }
+          >
+            https://ryucraftz-homepage.vercel.app/
+          </LinkButton>
+        </VStack>
+      </ScrollView>
+    </AnimatedColorBox>
+  )
+}
+
+export default AboutScreen
